40714295fa4e09747d86ecb8e00b3d9e68168f56
Umair <ext_umair.shahid@spark.net>
2025-06-01 20:14:18 +0200

    chore: config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-      <h1>F1 World Champions</h1>
+      <h1 className='text-3xl font-bold underline'>F1 World Champions</h1>
       <div className='card'>
         <button
           onClick={() => {
