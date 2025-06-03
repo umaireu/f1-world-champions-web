43b11617b8749f29ec3f26f952c7c2e48f2d9b28
Umair <umairshahidnl@gmail.com>
2025-06-03 15:56:41 +0200

    chore: update

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
+import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
+import App from './App';
 
 describe('App', () => {
   it('renders F1 World Champions heading', () => {
-    expect(true).toBeTruthy();
+    const { baseElement } = render(<App />);
+
+    expect(baseElement).toBeTruthy();
   });
 });
